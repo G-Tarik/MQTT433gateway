acafa571841ce93682f4d8462db9a70e80d233ea
Tarik <gtarik@gmail.com>
2025-02-14 22:30:32 +0100

    Fix deprecated platformio.managers

--- scripts/fw_version.py
+++ scripts/fw_version.py
@@ -2,14 +2,12 @@
 
 Project home: https://github.com/puuu/MQTT433gateway/
 """
-import os
 import subprocess
 import json
 import platformio
-from platformio.managers.lib import LibraryManager
-from platformio.managers.platform import PlatformFactory
+from platformio.package.manager.library import LibraryPackageManager
+from platformio.package.manager.platform import PlatformPackageManager
 Import("env")
-
 
 OUTFILE = "dist/version_build.h"
 TEMPLATE = """
@@ -26,23 +24,34 @@ def get_fw_version():
     except (subprocess.CalledProcessError, OSError):
         return "unknown"
 
+
 def get_dependencies():
-    lib_storage = os.path.join(env['PROJECTLIBDEPS_DIR'], env['PIOENV'])
-    lm = LibraryManager(lib_storage)
-    dependencies = {library['name']: library['version']
-                    for library in lm.get_installed()}
+    lib_mgr = LibraryPackageManager()
+    platform_mgr = PlatformPackageManager()
+
+    # Get installed libraries
+    dependencies = {
+        pkg.metadata.name: pkg.metadata.version
+        for pkg in lib_mgr.get_installed()
+    }
+
+    # Get platform information
     platform = env['PIOPLATFORM']
-    dependencies[platform] = PlatformFactory.newPlatform(platform).version
+    dependencies[platform] = str(platform_mgr.get_installed()[0].metadata.version)
     dependencies['PlatformIO'] = platformio.__version__
+
     return dependencies
+
 
 def get_dependencies_json():
     return json.dumps(get_dependencies(), separators=(',', ':'), sort_keys=True)
 
+
 def escape_string(text, escape_chars=CHARS_TO_ESCAPE):
     for char in escape_chars:
-        text = text.replace(char, '\\'+char)
+        text = text.replace(char, '\\' + char)
     return text
+
 
 def generate_file():
     with open(OUTFILE, 'w') as f:
