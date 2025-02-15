2bdd790e964b370776a83f3d0b38a40af774bfa7
Tarik <gtarik@gmail.com>
2025-02-15 15:44:54 +0100

    Create script build_web.sh

--- scripts/build_web.py
+++ scripts/build_web.py
@@ -4,31 +4,12 @@
 
 Project home: https://github.com/puuu/MQTT433gateway/
 """
-
 import os
-import platform
-from shutil import copyfile
-from subprocess import check_output, check_call, CalledProcessError
+from subprocess import check_output
 
 
-def is_tool(name):
-    cmd = "where" if platform.system() == "Windows" else "which"
-    try:
-        check_output([cmd, name])
-        return True
-    except CalledProcessError:
-        return False
-
-
-def build_web():
-    if not is_tool("npm"):
-        print("WARNING: npm is not avaiable. web interface will not be build.")
-        return
-    os.chdir("web")
-    print("Attempting to build webpage...")
-    check_call(["npm", "install"])
-    copyfile("build/index.html.gz.h", "../dist/index.html.gz.h")
-    os.chdir("..")
-
-
-build_web()
+if os.environ.get("PIO_BUILD_WEB") == "yes":
+    print("INFO: (re)building web ...")
+    check_output(["scripts/build_web.sh"])
+else:
+    print("INFO: skipping (re)building web")
